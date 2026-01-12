5e7cf2d2fe3e1f199a8ed9d771e84fc0fe0c25ce
trist007 <trist007@darkterminal.net>
2026-01-12 14:20:24 -0600

    poc moving spaceship

--- asteroids/code/win32_asteroids.cpp
+++ asteroids/code/win32_asteroids.cpp
@@ -8,25 +8,40 @@ int main(void)
     
     InitWindow(screenWidth, screenHeight, "asteroids");
     
+    Vector2 ballPosition = { (float)screenWidth/2, (float)screenHeight/2 };
+    
     SetTargetFPS(60);
     
     // main game loop
     while(!WindowShouldClose())
     {
-        // update
-        // update variables here
-        // draw
+        // Update
+        //-----------------------------------------------------------------------------------------
+        
+        if(IsKeyDown(KEY_RIGHT)) ballPosition.x += 2.0f;
+        if(IsKeyDown(KEY_LEFT)) ballPosition.x -= 2.0f;
+        if(IsKeyDown(KEY_UP)) ballPosition.y -= 2.0f;
+        if(IsKeyDown(KEY_DOWN)) ballPosition.y += 2.0f;
+        //-----------------------------------------------------------------------------------------
+        
+        
+        // Draw
+        //-----------------------------------------------------------------------------------------
         BeginDrawing();
+        
         {
             ClearBackground(RAYWHITE);
             
             DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
             
+            DrawCircleV(ballPosition, 50, MAROON);
         }
+        
         EndDrawing();
+        //-----------------------------------------------------------------------------------------
     }
     
-    // de-initialization
+    // De-Initialization
     CloseWindow();
     
     return(0);
