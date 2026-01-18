703b4f326e4a6ba55d2cb99f7336e3493754563e
trist007 <trist007@darkterminal.net>
2026-01-18 10:30:04 -0600

    increased asteroid speed every 10 secs, fixed spawn, and added game over screen

--- asteroids/code/win32_asteroids.cpp
+++ asteroids/code/win32_asteroids.cpp
@@ -16,6 +16,16 @@ int main(void)
     
     // Variables
     
+    bool gameOver = false;
+    
+    // Timer
+    float gameTimer = 0.0f;
+    float speedIncreaseInterval = 10.0f;
+    float asteroidSpeedMultiplier = 1.0f;
+    
+    float asteroidSpawnTimer = 0.0f;
+    float asteroidSpawnInterval = 1.0f;
+    
     // Player ship
     Vector2 shipPosition = { (float)screenWidth/2, (float)screenHeight/2 };
     Vector2 shipVelocity = { 0.0f, 0.0f };
@@ -32,7 +42,7 @@ int main(void)
     Vector2 asteroidPos[MAX_ASTEROIDS];
     Vector2 asteroidVelocity[MAX_ASTEROIDS];
     Vector2 asteroidDirection[MAX_ASTEROIDS];
-    float asteroidScale[MAX_ASTEROIDS];
+    int asteroidScale[MAX_ASTEROIDS];
     bool asteroidSpawned[MAX_ASTEROIDS];
     float asteroidSpeed = 2.0f;
     Vector2 asteroidTarget = { screenWidth / 2.0f, screenHeight / 2.0f };
@@ -66,127 +76,167 @@ int main(void)
         //-----------------------------------------------------------------------------------------
         // Update
         //-----------------------------------------------------------------------------------------
-        if(IsKeyPressed(KEY_H))
+        if(!gameOver)
         {
-            if(IsCursorHidden()) ShowCursor();
-            else HideCursor();
-        }
-        
-        // Control ship with keyboard
-        if(IsKeyDown(KEY_RIGHT)) shipRotation += 0.05f;
-        if(IsKeyDown(KEY_LEFT)) shipRotation -= 0.05f;
-        if(IsKeyDown(KEY_UP))
-        {
-            shipVelocity.x += sinf(shipRotation) * thrust;
-            shipVelocity.y -= cosf(shipRotation) * thrust;
-        }
-        if(IsKeyDown(KEY_DOWN))
-        {
-            shipVelocity.x -= sinf(shipRotation) * thrust;
-            shipVelocity.y += cosf(shipRotation) * thrust;
-        }
-        if(IsKeyPressed(KEY_SPACE))
-        {
+            if(IsKeyPressed(KEY_H))
+            {
+                if(IsCursorHidden()) ShowCursor();
+                else HideCursor();
+            }
+            
+            // Control ship with keyboard
+            if(IsKeyDown(KEY_RIGHT)) shipRotation += 0.05f;
+            if(IsKeyDown(KEY_LEFT)) shipRotation -= 0.05f;
+            if(IsKeyDown(KEY_UP))
+            {
+                shipVelocity.x += sinf(shipRotation) * thrust;
+                shipVelocity.y -= cosf(shipRotation) * thrust;
+            }
+            if(IsKeyDown(KEY_DOWN))
+            {
+                shipVelocity.x -= sinf(shipRotation) * thrust;
+                shipVelocity.y += cosf(shipRotation) * thrust;
+            }
+            if(IsKeyPressed(KEY_SPACE))
+            {
+                for(int i = 0;
+                    i < MAX_BULLETS;
+                    i++)
+                {
+                    if(!bulletActive[i])
+                    {
+                        bulletActive[i] = true;
+                        bulletPosition[i] = shipPosition;
+                        bulletVelocity[i].x = sinf(shipRotation) * bulletSpeed;
+                        bulletVelocity[i].y = -cosf(shipRotation) * bulletSpeed;
+                        break;
+                    }
+                }
+            }
+            
+            // Adjust asteroid speed
+            gameTimer += GetFrameTime();
+            if(gameTimer >= speedIncreaseInterval)
+            {
+                asteroidSpeedMultiplier += 0.5f;
+                gameTimer = 0.0f;
+            }
+            
+            // Apply velocity and friction to shipPosition
+            shipPosition.x += shipVelocity.x;
+            shipPosition.y += shipVelocity.y;
+            
+            shipVelocity.x *= friction;
+            shipVelocity.y *= friction;
+            
+            // Update active bullets
             for(int i = 0;
                 i < MAX_BULLETS;
                 i++)
             {
-                if(!bulletActive[i])
+                if(bulletActive[i])
                 {
-                    bulletActive[i] = true;
-                    bulletPosition[i] = shipPosition;
-                    bulletVelocity[i].x = sinf(shipRotation) * bulletSpeed;
-                    bulletVelocity[i].y = -cosf(shipRotation) * bulletSpeed;
-                    break;
-                }
-            }
-        }
-        
-        // Apply velocity and friction to shipPosition
-        shipPosition.x += shipVelocity.x;
-        shipPosition.y += shipVelocity.y;
-        
-        shipVelocity.x *= friction;
-        shipVelocity.y *= friction;
-        
-        // Update active bullets
-        for(int i = 0;
-            i < MAX_BULLETS;
-            i++)
-        {
-            if(bulletActive[i])
-            {
-                bulletPosition[i].x += bulletVelocity[i].x;
-                bulletPosition[i].y += bulletVelocity[i].y;
-                
-                // Deactive if off screen
-                if(bulletPosition[i].x < 0 || bulletPosition[i].x > screenWidth ||
-                   bulletPosition[i].y < 0 || bulletPosition[i].y > screenHeight)
-                {
-                    bulletActive[i] = false;
-                }
-            }
-        }
-        
-        // Spawn asteroids
-        for(int i = 0;
-            i < MAX_ASTEROIDS;
-            i++)
-        {
-            if(!asteroidSpawned[i])
-            {
-                // Get random angle and scale for asteroid
-                float angle = GetRandomValue(0, 360) * DEG2RAD;
-                float spawnRadius = screenRadius + 50.0f;
-                
-                asteroidPos[i].x = screenWidth / 2.0f + cosf(angle) * spawnRadius;
-                asteroidPos[i].y = screenWidth / 2.0f + sinf(angle) * spawnRadius;
-                
-                asteroidScale[i] = GetRandomValue(10.0f, 80.0f);
-                asteroidSpawned[i] = true;
-                asteroidDirection[i] = Vector2Normalize(Vector2Subtract(asteroidTarget, asteroidPos[i]));
-                asteroidVelocity[i] = Vector2Scale(asteroidDirection[i], asteroidSpeed);
-            }
-        }
-        
-        
-        // Update spawned asteroids
-        for(int i = 0;
-            i < MAX_ASTEROIDS;
-            i++)
-        {
-            if(asteroidSpawned[i])
-            {
-                asteroidPos[i].x += asteroidVelocity[i].x;
-                asteroidPos[i].y += asteroidVelocity[i].y;
-                
-                if(asteroidPos[i].x < 0 || asteroidPos[i].x > screenWidth ||
-                   asteroidPos[i].y < 0 || asteroidPos[i].y > screenHeight)
-                {
-                    asteroidSpawned[i] = false;
-                }
-            }
-        }
-        
-        // Check for collisions
-        for(int i = 0;
-            i < MAX_BULLETS;
-            i++)
-        {
-            if(bulletActive[i])
-            {
-                for(int j = 0;
-                    j< MAX_ASTEROIDS;
-                    j++)
-                {
-                    if(asteroidSpawned[j])
+                    bulletPosition[i].x += bulletVelocity[i].x;
+                    bulletPosition[i].y += bulletVelocity[i].y;
+                    
+                    // Deactive if off screen
+                    if(bulletPosition[i].x < 0 || bulletPosition[i].x > screenWidth ||
+                       bulletPosition[i].y < 0 || bulletPosition[i].y > screenHeight)
                     {
-                        float distance = Vector2Distance(bulletPosition[i], asteroidPos[j]);
+                        bulletActive[i] = false;
+                    }
+                }
+            }
+            
+            asteroidSpawnTimer += GetFrameTime();
+            
+            if(asteroidSpawnTimer >= asteroidSpawnInterval)
+            {
+                
+                // Spawn asteroids
+                for(int i = 0;
+                    i < MAX_ASTEROIDS;
+                    i++)
+                {
+                    if(!asteroidSpawned[i])
+                    {
+                        // Get random angle and scale for asteroid
+                        float angle = GetRandomValue(0, 360) * DEG2RAD;
+                        float spawnRadius = screenRadius + 50.0f;
                         
-                        if(distance < (asteroidScale[j] + bulletRadius))
+                        asteroidPos[i].x = screenWidth / 2.0f + cosf(angle) * spawnRadius;
+                        asteroidPos[i].y = screenHeight / 2.0f + sinf(angle) * spawnRadius;
+                        
+                        asteroidScale[i] = GetRandomValue(10, 80);
+                        asteroidSpawned[i] = true;
+                        asteroidDirection[i] = Vector2Normalize(Vector2Subtract(asteroidTarget, asteroidPos[i]));
+                        asteroidVelocity[i] = Vector2Scale(asteroidDirection[i], asteroidSpeed * asteroidSpeedMultiplier);
+                        
+                        // Only spawn one asteroid per interval
+                        asteroidSpawnTimer = 0.0f;
+                        break;
+                    }
+                }
+            }
+            
+            // Update spawned asteroids
+            for(int i = 0;
+                i < MAX_ASTEROIDS;
+                i++)
+            {
+                if(asteroidSpawned[i])
+                {
+                    asteroidPos[i].x += asteroidVelocity[i].x;
+                    asteroidPos[i].y += asteroidVelocity[i].y;
+                    
+                    float margin = 200.0f;
+                    if(asteroidPos[i].x < -margin || asteroidPos[i].x > screenWidth + margin ||
+                       asteroidPos[i].y < -margin || asteroidPos[i].y > screenHeight + margin)
+                    {
+                        asteroidSpawned[i] = false;
+                    }
+                }
+            }
+            
+            // Check for bullet asteroid collisions
+            for(int i = 0;
+                i < MAX_BULLETS;
+                i++)
+            {
+                if(bulletActive[i])
+                {
+                    for(int j = 0;
+                        j< MAX_ASTEROIDS;
+                        j++)
+                    {
+                        if(asteroidSpawned[j])
                         {
-                            bulletActive[i] = false;
-                            asteroidSpawned[j] = false;
+                            float distance = Vector2Distance(bulletPosition[i], asteroidPos[j]);
+                            
+                            if(distance < (asteroidScale[j] + bulletRadius))
+                            {
+                                bulletActive[i] = false;
+                                asteroidSpawned[j] = false;
+                            }
+                        }
+                    }
+                }
+            }
+            
+            // Check for asteroid player collisions
+            if(!gameOver)
+            {
+                for(int i = 0;
+                    i < MAX_ASTEROIDS;
+                    i++)
+                {
+                    if(asteroidSpawned[i])
+                    {
+                        float distance = Vector2Distance(asteroidPos[i], shipPosition);
+                        if(distance < (asteroidScale[i] + shipSize))
+                        {
+                            gameOver = true;
+                            break;
                         }
                     }
                 }
@@ -244,6 +294,14 @@ int main(void)
             if(IsCursorHidden()) DrawText("CURSOR HIDDEN", 20, 60, 20, RED);
             else DrawText("CURSOR VISIBLE", 20, 60, 20, LIME);
             
+        }
+        
+        if(gameOver)
+        {
+            int fontSize = 80;
+            char *text = "GAME OVER";
+            int textWidth = MeasureText(text, fontSize);
+            DrawText(text, screenWidth / 2 - textWidth / 2, screenHeight / 2 - fontSize / 2, fontSize, RED);
         }
         
         EndDrawing();
